e0ec5c42e9a6ca84f2ec68965ae1f7557af97775
veragisella38-cmd <veragisella38@gmail.com>
2026-08-26 07:11:57 -0500

    Tarea matriz 5x5

---
+++ matriz5x5.py
@@ -0,0 +1,15 @@
+matriz = [[0 for _ in range(5)] for _ in range(5)]
+
+for i in range(5):
+    for j in range(5):
+        valor = int(
+            input(f"Ingrese el valor para la posición [{i}][{j}]: ")
+        )
+        matriz[i][j] = valor
+
+print("\nMatriz ingresada:")
+
+for i in range(5):
+    for j in range(5):
+        print(matriz[i][j], end="\t")
+    print()
